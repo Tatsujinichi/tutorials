0933c6560ab0354f8a1a509066e5103a04a12ded
playerone <tatsujinichi@gmail.com>
2014-05-06 21:33:45 -0400

    previous changes, uncommited

--- EclipseWorkspace/JOGLTutorial/src/jOGLPackage/XMLParser.java
+++ EclipseWorkspace/JOGLTutorial/src/jOGLPackage/XMLParser.java
@@ -19,17 +19,43 @@ public class XMLParser<T>
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		Document doc = builder.parse(path);
-		Element element = doc.getDocumentElement();
-		NodeList nodeList = element.getChildNodes();
+		doc.getDocumentElement().normalize();
+		//Element element = doc.getDocumentElement();
 		
-
-
+		Node library_geometries = doc.getElementsByTagName("library_geometries").item(0);
+		Node geometries = library_geometries.get
+		Node mesh = geometries.getFirstChild();
+		NodeList sources = mesh.getChildNodes();
 		
-		int length = nodeList.getLength();
-		System.out.println(length);
-		for(int i = 0; i < length; i++)
+		System.out.println(sources.getLength());
+		for(int i = 0; i < sources.getLength(); i++)
 		{
-			System.out.println(nodeList.item(i).toString());
+			Node node = sources.item(i);
+			System.out.println(node.getNodeName());
+			if (node.getNodeType() == Node.ELEMENT_NODE) 
+			{	 
+				Element element = (Element)node;
+				if(element.hasAttribute("Cube-mesh-positions"))
+				{
+					int count = Integer.parseInt(element.getAttribute("count"));
+					NodeList floatelements = element.getElementsByTagName("float_array");
+					Node floatNode = floatelements.item(0);
+					Element floatElement = (Element)floatNode;
+					String stringFloatArray = floatElement.getTextContent();
+					System.out.println(stringFloatArray);
+					//float_array
+				}
+			}
 		}
 	}
+	
+	private static String getValue(String tag, Element element) 
+	{
+		NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
+		Node node = (Node) nodes.item(0);
+		return node.getNodeValue();
+	}
+
+
+		//Read more: http://javarevisited.blogspot.com/2011/12/parse-xml-file-in-java-example-tutorial.html#ixzz2xstzickE
 }
